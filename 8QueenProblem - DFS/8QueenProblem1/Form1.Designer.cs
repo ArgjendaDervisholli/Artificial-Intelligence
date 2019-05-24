2d13fd51d0870a6a0ecd4f08cb8d58748e8e1ca0
ArgjendaDervisholli <argjendadervisholli@hotmail.com>
2019-05-24 17:00:15 +0200

    Add files via upload

---
+++ 8QueenProblem - DFS/8QueenProblem1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace _8QueenProblem1
+{
+    partial class QueenProblem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nrOfBlockedPos = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblPositionXY = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nrOfBlockedPos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.ForestGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(333, 62);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(736, 578);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 50);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(107, 30);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nr. of Queens";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(397, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 5;
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xTextBox.Location = new System.Drawing.Point(12, 126);
+            this.xTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(107, 30);
+            this.xTextBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 95);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "First Queen position";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(125, 124);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 33);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Try";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nrOfBlockedPos
+            // 
+            this.nrOfBlockedPos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nrOfBlockedPos.Location = new System.Drawing.Point(12, 206);
+            this.nrOfBlockedPos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nrOfBlockedPos.Name = "nrOfBlockedPos";
+            this.nrOfBlockedPos.Size = new System.Drawing.Size(107, 30);
+            this.nrOfBlockedPos.TabIndex = 13;
+            this.nrOfBlockedPos.ValueChanged += new System.EventHandler(this.nrOfBlockedPos_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 161);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(294, 29);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Nr. of blocked positions";
+            // 
+            // lblPositionXY
+            // 
+            this.lblPositionXY.AutoSize = true;
+            this.lblPositionXY.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblPositionXY.Location = new System.Drawing.Point(13, 255);
+            this.lblPositionXY.Name = "lblPositionXY";
+            this.lblPositionXY.Size = new System.Drawing.Size(0, 18);
+            this.lblPositionXY.TabIndex = 16;
+            // 
+            // QueenProblem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1004, 538);
+            this.Controls.Add(this.lblPositionXY);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nrOfBlockedPos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "QueenProblem";
+            this.Text = "8QueenProblem";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.QueenProblem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nrOfBlockedPos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown nrOfBlockedPos;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblPositionXY;
+    }
+}
+
---
+++ 8QueenProblem - DFS/8QueenProblem1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _8QueenProblem1
+{
+    public partial class QueenProblem : Form
+    {
+        static int N;
+        int[,] shahu;
+        int locationX = 0;
+        int locationY = 0;
+        int boxSize = 50;
+        int paddingBox = 10;
+        int x = 1;
+        PictureBox picture;
+        public QueenProblem()
+        {
+            InitializeComponent();
+            xTextBox.Text = "1";
+            luaj();
+
+        }
+        private void btnSolve_Click(object sender, EventArgs e) { }
+
+        public bool VendosMbretreshen(int q, int N) // q- cilen mbretresh qe njekohesisht tregon edhe rreshtin, N sa eshte matrica e madhe
+        {
+            if (q == N)
+                return true;
+            for (int i = 0; i < N; i++)
+            {
+                // kontrollojm a guxojm me pozicionu ne rreshtin i kolonen q
+                if (kontrolloVendosjen(i, q))
+                {
+                    if (shahu[i, q] != -1)
+                    {
+                        shahu[i, q] = 1;
+                        if (VendosMbretreshen(q + 1, N))
+                            return true;
+                        shahu[i, q] = 0;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool kontrolloVendosjen(int rr, int k) //Rreshti dhe Kolona
+        {
+            for (int j = 0; j < k; j++)
+                if (shahu[rr, j] == 1)
+                    return false;
+
+            for (int i = rr + 1, j = k - 1; i < shahu.GetLength(0) && j >= 0; i++, j--)
+            {
+                if (shahu[i, j] == 1)
+                    return false;
+            }
+
+            for (int i = rr - 1, j = k - 1; i >= 0 && j >= 0; i--, j--)
+                if (shahu[i, j] == 1)
+                    return false;
+
+            return true;
+        }
+        public void vizatoBox(int i, int j, bool mbreteresha)
+        {
+            pictureBox1.Size = new System.Drawing.Size((N * boxSize + 20), (N * boxSize + 20));
+            pictureBox1.Location = new System.Drawing.Point(300, 50);
+            pictureBox1.Visible = true;
+            picture = new PictureBox
+            {
+                Name = "pictureBox",
+                Size = new Size(boxSize, boxSize),
+                Location = new Point(locationX + boxSize * j + paddingBox, locationY + boxSize * i + paddingBox)
+            };
+            if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1))
+                picture.BackColor = System.Drawing.Color.Black;
+            else
+                picture.BackColor = System.Drawing.Color.White;
+            if (mbreteresha == true)
+                picture.Image = Image.FromFile(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"..\..\..\Images\BQueen.gif");
+            if (shahu[i, j] == -1 && mbreteresha == false)
+            {
+                picture.BackColor = System.Drawing.Color.Black;
+                picture.Image = Image.FromFile(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"..\..\..\Images\x.png");
+            }
+            pictureBox1.Controls.Add(picture);
+        }
+        public void fshij()
+        {
+            pictureBox1.Controls.Clear();
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            lblPositionXY.Text = "";
+            luaj((int)nrOfBlockedPos.Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblPositionXY.Text = "";
+            luaj((int)nrOfBlockedPos.Value);
+        }
+
+        private void nrOfBlockedPos_ValueChanged(object sender, EventArgs e)
+        {
+            lblPositionXY.Text = "";
+            luaj((int)nrOfBlockedPos.Value);
+        }
+
+        public void luaj(int numri=0)
+        {
+            fshij();
+            N = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+            shahu = new int[N, N];
+            for (int i = 0; i < shahu.GetLength(0); i++)
+                for (int j = 0; j < shahu.GetLength(0); j++)
+                {
+                    shahu[i, j] = 0;
+                }
+
+            for (int k = 0; k < numri; k++)
+            {
+                Random rnd = new Random();
+                int i = rnd.Next(0, N);
+                Thread.Sleep(50);
+                int j = rnd.Next(0, N);
+                if (shahu[i, j] != -1)
+                {
+                    shahu[i, j] = -1;
+                    lblPositionXY.Text = lblPositionXY.Text + "i: " +i+ ", j: " +j+ "\n";
+                }
+                else
+                    k--;
+            }
+
+            if (Convert.ToInt32(xTextBox.Text) < N)
+            {
+                shahu[Convert.ToInt32(xTextBox.Text), 0] = 1;
+                if (VendosMbretreshen(1, N) == true)
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            if (shahu[i, j] == 1)
+                                vizatoBox(i, j, true);
+                            else
+                                vizatoBox(i, j, false);
+                        }
+                    }
+                    label2.ForeColor = System.Drawing.Color.Green;
+                    label2.Text = "Solution exists!";
+                }
+                else
+                {
+                    label2.ForeColor = System.Drawing.Color.Red;
+                    label2.Text = "Solution doesn't exists!";
+                }
+            }
+        }
+
+        private void QueenProblem_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
